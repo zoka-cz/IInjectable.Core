d3f6e7df8598c39c5823f1e606a64c33b11907bc
Michal Linhart <michal.linhart@gmail.com>
2021-06-23 11:05:48 +0200

    * implementation

---
+++ _src/IInjectable.Core/DependencyInjectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Zoka.IInjectable
+{
+	/// <summary>Extenstion to the IServiceCollection for IInjectable</summary>
+	public static class DependencyInjectionExtensions
+	{
+		/// <summary>When used during Startup.ConfigureServices, it registers all types derived from IInjectable as ScopedService</summary>
+		public static IServiceCollection					RegisterInjectables(this IServiceCollection _service_collection)
+		{
+			foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies().Union(AppDomain.CurrentDomain.ReflectionOnlyGetAssemblies()))
+			{
+				foreach (Type t in a.GetTypes())
+				{
+					if (typeof(IInjectable).IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface)
+						_service_collection.AddScoped(t);
+				}
+			}
+
+			return _service_collection;
+		}
+	}
+}
---
+++ _src/IInjectable.Core/IInjectable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoka.IInjectable
+{
+	/// <summary>Interface, which marks any implementing class as injectable. It may be then registered as ScopedService and used inside the project.</summary>
+	interface IInjectable
+	{
+	}
+}
